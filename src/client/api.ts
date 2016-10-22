ab55375000dc5ec67bd3edc637c14faa9a27527a
sergeyt <stodyshev@gmail.com>
2016-10-23 02:56:46 +0700

    use es6 promise

--- src/client/api.ts
+++ src/client/api.ts
@@ -1,6 +1,4 @@
-import * as Promise from 'promise';
-
-export function fetchJSON<T>(path: string): Promise.IThenable<T> {
+export function fetchJSON<T>(path: string): Promise<T> {
   // TODO fix typings
   return (window as any).fetch(path).then(res => res.json());
 }
