35013c80f0f8736418f8159bdde4ad3a70197eb4
Sergey Zhirov <sergey@worldsofcubes.net>
2016-10-26 12:20:35 +0700

    Semantic ui example modified to become PageHeader's prototype (issue #12)

--- src/client/pages/sui-example.tsx
+++ src/client/pages/sui-example.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Input, Menu } from 'semantic-ui-react';
+import { Input, Menu, Dropdown, Container } from 'semantic-ui-react';
 
 interface PageState {
   activeItem: string;
@@ -10,22 +10,41 @@ export default class Page extends React.Component<{}, PageState> {
     activeItem: 'home',
   };
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+  items = {};
+
+  handleItemClick = (name) => this.setState({ activeItem: name });
+  handleMenuItemClick = (e, { name }) => this.handleItemClick(name);
+  handleDropdownItemClick = (e, { value }) => this.handleItemClick(value);
 
   render() {
     const { activeItem } = this.state;
 
+    const user =  {
+      id: '',
+      name: '%username%',
+      login: '',
+      role: 'admin',
+    };//TODO fetch real logged in user's information
     return (
       <Menu>
-        <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-        <Menu.Item name='calendars' active={activeItem === 'calendars'} onClick={this.handleItemClick} />
-        <Menu.Item name='teams' active={activeItem === 'teams'} onClick={this.handleItemClick} />
-        <Menu.Menu position='right'>
-          <Menu.Item>
+        <Container>
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleMenuItemClick} />
+          <Menu.Item name='calendars' active={activeItem === 'calendars'} onClick={this.handleMenuItemClick} />
+          <Menu.Item name='reports' active={activeItem === 'reports'} onClick={this.handleMenuItemClick}/>
+          <Menu.Item name='teams' active={activeItem === 'teams'} onClick={this.handleMenuItemClick}/>
+          {user.role === 'admin' &&
+          <Menu.Item name='admin' active={activeItem === 'admin'} onClick={this.handleMenuItemClick}/>
+          }
+          <Menu.Item position='right'>
             <Input icon='search' placeholder='Search...' />
           </Menu.Item>
-          <Menu.Item name='logout' active={activeItem === 'logout'} onClick={this.handleItemClick} />
-        </Menu.Menu>
+          <Dropdown as={Menu.Item} text={user.name}>
+            <Dropdown.Menu>
+              <Dropdown.Item value='options' text="My account" active={activeItem === 'options'} onClick={this.handleDropdownItemClick} />
+              <Dropdown.Item value='logout' text="Log Out" active={activeItem === 'logout'} onClick={this.handleDropdownItemClick} />
+            </Dropdown.Menu>
+          </Dropdown>
+        </Container>
       </Menu>
     );
   }
