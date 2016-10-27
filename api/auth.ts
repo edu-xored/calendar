99e950d214bec3fe8a0b11e07927d19654430b33
moonlightnvkz <moonlightnvkz@users.noreply.github.com>
2016-10-27 23:44:49 +0700

    Added async. Don't sure whether I've done it right.

--- api/auth.ts
+++ api/auth.ts
@@ -2,18 +2,22 @@ import * as express from 'express';
 import resolveUser from '../ldap/client';
 import {User} from '../src/lib/model';
 
-export function login(req: express.Request, res: express.Response): void {
+export async function login(req: express.Request, res: express.Response) {
     let login: string = req.body.login;
     let pwd: string = req.body.pwd;
-    resolveUser(login, pwd, (error: string, user: User) => {
+    try {
+    await resolveUser(login, pwd, (error: string, user: User) => {
         if (error) {
             res.send(error);
         } else {
             res.status(200).send(user);
         }
     });
+    } catch (error) {
+        res.send(error);
+    }
 }
 
-export function logout(req: express.Request, res: express.Response): void {
-    res.end();
+export async function logout(req: express.Request, res: express.Response) {
+    res.sendStatus(200);
 };
