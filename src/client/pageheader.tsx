050dd4c627fdec508aceee378125435b6e23991f
Sergey Zhirov <sergey@webmcrex.com>
2016-11-01 01:19:47 +0700

    Fixed build failing (issue #12)

--- src/client/pageheader.tsx
+++ src/client/pageheader.tsx
@@ -10,8 +10,8 @@ interface MenuItem {
   name: string;
   text: string;
   role: boolean | string;
-  parent: boolean | string,
-  rightSide: boolean,
+  parent: boolean | string;
+  rightSide: boolean;
 }
 
 export default class PageHeader extends React.Component<{}, PageState> {
@@ -24,15 +24,15 @@ export default class PageHeader extends React.Component<{}, PageState> {
   handleDropdownItemClick = (e, {value}) => this.handleItemClick(value);
 
   user: User = {
-  id: '1',
-  login: '%username%',
-  name: '%username%',
-  pwdhash: '%username%', // password hash
-  avatar: '', // URL to avatar image, e.g. it could be a gravatar URL or URL to uploaded image
-  role: 'admin',
-  position: '213',
-  place: 's',
-  };//TODO fetch real logged in user's information
+    id: '1',
+    login: '%username%',
+    name: '%username%',
+    pwdhash: '%username%', // password hash
+    avatar: '', // URL to avatar image, e.g. it could be a gravatar URL or URL to uploaded image
+    role: 'admin',
+    position: '213',
+    place: 's',
+  }; // TODO fetch real logged in user's information
 
   menu: MenuItem[] = [
     {
@@ -93,68 +93,68 @@ export default class PageHeader extends React.Component<{}, PageState> {
     },
   ];
 
-  showItem (item, child = false) {
-    if (item.parent == item.name)
+  showItem(item, child = false) {
+    if (item.parent === item.name)
       return;
-    if ((item.role === true && +(this.user.id) <= 0) || (item.role && item.role == this.user.role))//TODO normal check for rights
+    if ((item.role === true && +(this.user.id) <= 0) || (item.role && item.role === this.user.role)) // TODO normal check for rights
       return;
-    var dropdown = [];
-    for (var tmpitem of this.menu)
-      if (tmpitem.parent == item.name)
+    let dropdown = [];
+    for (let tmpitem of this.menu)
+      if (tmpitem.parent === item.name)
         dropdown.push(this.showItem(tmpitem));
     if (dropdown.length > 0) {
       return (
-        <Dropdown as={(child)? Dropdown.Item: Menu.Item} text={item.name}>
+        <Dropdown as={(child) ? Dropdown.Item : Menu.Item} text={item.text}>
           <Dropdown.Menu>
             {dropdown}
           </Dropdown.Menu>
         </Dropdown>
-      )
+      );
     } else {
       if (child)
         return (
-          <Dropdown.Item value={item.name} text={item.text} active={this.state.activeItem === item.name} onClick={this.handleDropdownItemClick} />
-        )
+          <Dropdown.Item value={item.name} text={item.text} active={this.state.activeItem === item.name}
+                         onClick={this.handleDropdownItemClick}/>
+        );
       else
         return (
           <Menu.Item name={item.name} active={this.state.activeItem === item.name} onClick={this.handleMenuItemClick}/>
-        )
+        );
     }
   }
 
   render() {
-
-    var container = [];
-    for (var item of this.menu)
-      if (item.parent == false && !item.rightSide)
+    let container = [];
+    for (let item of this.menu)
+      if (item.parent === false && !item.rightSide)
         container.push(this.showItem(item));
     container.push(
       <Menu.Item position='right'>
-        <Input icon='search' placeholder='Search...' />
+        <Input icon='search' placeholder='Search...'/>
       </Menu.Item>
     );
-    for (var item of this.menu)
-      if (item.parent == false && item.rightSide)
+    for (let item of this.menu)
+      if (item.parent === false && item.rightSide)
         container.push(this.showItem(item));
     return (
       <Menu><Container>{container}</Container></Menu>
     );
     /*
      <Container>
-     <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleMenuItemClick} />
-     <Menu.Item name='calendars' active={activeItem === 'calendars'} onClick={this.handleMenuItemClick} />
-     <Menu.Item name='reports' active={activeItem === 'reports'} onClick={this.handleMenuItemClick}/>
-     <Menu.Item name='teams' active={activeItem === 'teams'} onClick={this.handleMenuItemClick}/>
-     {user.role === 'admin' &&
-     <Menu.Item name='admin' active={activeItem === 'admin'} onClick={this.handleMenuItemClick}/>
+     <Menu.Item name='home' active={this.state.activeItem === 'home'} onClick={this.handleMenuItemClick} />
+     <Menu.Item name='calendars' active={this.state.activeItem === 'calendars'} onClick={this.handleMenuItemClick} />
+     <Menu.Item name='reports' active={this.state.activeItem === 'reports'} onClick={this.handleMenuItemClick}/>
+     <Menu.Item name='teams' active={this.state.activeItem === 'teams'} onClick={this.handleMenuItemClick}/>
+     {this.user.role === 'admin' &&
+     <Menu.Item name='admin' active={this.state.activeItem === 'admin'} onClick={this.handleMenuItemClick}/>
      }
      <Menu.Item position='right'>
      <Input icon='search' placeholder='Search...' />
      </Menu.Item>
-     <Dropdown as={Menu.Item} text={user.name}>
+     <Dropdown as={Menu.Item} text={this.user.name}>
      <Dropdown.Menu>
-     <Dropdown.Item value='options' text="My account" active={activeItem === 'options'} onClick={this.handleDropdownItemClick} />
-     <Dropdown.Item value='logout' text="Log Out" active={activeItem === 'logout'} onClick={this.handleDropdownItemClick} />
+     <Dropdown.Item value='options' text="My account" active={this.state.activeItem === 'options'} onClick={this.handleDropdownItemClick} />
+     <Dropdown.Item value='logout' text="Log Out" active={this.state.activeItem === 'logout'} onClick={this.handleDropdownItemClick} />
      </Dropdown.Menu>
      </Dropdown>
      </Container>
