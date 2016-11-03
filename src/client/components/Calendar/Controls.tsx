9b99e71350c1f3d4875480430c501d6c97588993
Alexey Boltava <goodmoveab@gmail.com>
2016-11-03 13:07:55 +0700

    Create grid scaffold (issue #44)
    Implemented a shallow version of calendar grid on fake data: grid with scrollbars enabled and sticky header row. Added route /calendar/* for test view.

--- src/client/components/Calendar/Controls.tsx
+++ src/client/components/Calendar/Controls.tsx
@@ -2,5 +2,11 @@ import * as React from 'react';
 
 
 export default class Controls extends React.Component<any, any> {
-
+  render() {
+    return (
+      <div>
+        I am Controls component!
+      </div>
+    );
+  }
 }
--- src/client/routes.tsx
+++ src/client/routes.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { Router, Route, IndexRoute, RouterState } from 'react-router';
 import history from './history';
+import Calendar from './pages/calendar';
 import Home from './pages/home';
 import Blank from './pages/blank';
 
 const Routes = (
   <Router history={history}>
     <Route path="/" component={Home}/>
+    <Route path="/calendar/*" component={Calendar}/>
     <Route path="*" component={Blank}/>
   </Router>
 );
