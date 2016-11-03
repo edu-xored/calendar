7eb4db3170d108cd13a0d1dbe9f0c485a7b98737
Alexey Boltava <goodmoveab@gmail.com>
2016-11-03 13:07:55 +0700

    Create grid scaffold (issue #44)
    Implemented a shallow version of calendar grid on fake data: grid with scrollbars enabled and sticky header row. Added route /calendar/* for test view.

--- src/client/components/Calendar/CalendarGrid.tsx
+++ src/client/components/Calendar/CalendarGrid.tsx
@@ -1,6 +1,61 @@
 import * as React from 'react';
 
+import Row from './Row';
+
+import '../../styles/calendar-grid';
 
 export default class CalendarGrid extends React.Component<any, any> {
-  
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    let grid = document.getElementsByClassName('grid-body-wrapper')[0];
+    grid.addEventListener('scroll', this.handleOnGridScroll);
+  }
+
+  componentWillUnmount() {
+    let grid = document.getElementsByClassName('grid-body-wrapper')[0]
+    grid.removeEventListener('scroll', this.handleOnGridScroll);
+  }
+
+  render() {
+    const members = ["Alex", "Bill", "Jack", "William", "John", "Rob", "Sam", "Bob", "Steve", "Tim"];
+    const headerLength = 31;
+    const tmCount = members.length;
+
+    let headerRow = <Row id={'header-row'} rowData={ [...Array(headerLength+1).keys()] } />;
+    let teamMembersRows = members.map((member, i) =>
+        <Row key={ i } id={ `team-row-${i}` } rowData={ [member] } />
+    );
+    let eventsRows = [...Array(tmCount).keys()].map((i) =>
+        <Row key={ i } id={`event-row-${i}`} rowData={ [...Array(headerLength).keys()] } />
+    );
+
+    return (
+      <div className='calendar-grid'>
+        <div className='grid-header'>
+          { headerRow }
+        </div>
+        <div className='grid-body-wrapper'>
+          <div className='grid-body'>
+            <div className='team-members-column'>
+              { teamMembersRows }
+            </div>
+            <div className='events-list-column'>
+              { eventsRows }
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  handleOnGridScroll() {
+    let grid: any, gridHeader: any;
+
+    grid = document.getElementsByClassName('grid-body-wrapper')[0];
+    gridHeader = document.getElementsByClassName('grid-header')[0];
+    gridHeader.style.left = `${-grid.scrollLeft}px`;
+  }
 }
--- src/client/components/Calendar/Filter.tsx
+++ src/client/components/Calendar/Filter.tsx
@@ -2,5 +2,11 @@ import * as React from 'react';
 
 
 export default class Filter extends React.Component<any, any> {
-
+  render() {
+    return (
+      <div>
+        I am Filter component!
+      </div>
+    );
+  }
 }
