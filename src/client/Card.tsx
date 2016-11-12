f0292a7e0f835c864d2b38a13e113eedde3ff50a
Roman Zakharov <devilrigoll@gmail.com>
2016-11-12 21:58:53 +0700

    Ready card without style

--- src/client/Card.tsx
+++ src/client/Card.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
-import { render } from 'react-dom';
+import * as ReactDOM from "react-dom";
+import "./card.css"
 
-export interface CardProps {username_:string; role_:string; image_src_:string; command_: string; position_:string; place_: string; script_: string;}
+export interface CardProps {side_:string; username_:string; role_:string; image_src_:string; command_: string; position_:string; place_: string; script_: string;}
 
 interface Front_cardProps{username_:string; role_:string; image_src_:string;}
 interface Back_cardProps{command_: string; position_:string; place_: string; script_: string;}
+interface IState_card{side_card_flag:string}
 
-
+//constants
+export var FRONT_SIDE = "front_side";
+export var BACK_SIDE = "back_side";
 
 //export class Card extends React.Component<CardProps, {}> {
   //  render() {
@@ -17,10 +21,10 @@ interface Back_cardProps{command_: string; position_:string; place_: string; scr
 class Front_card extends React.Component<Front_cardProps, {}> {
   render() {
   return (
-      <div>
-            <img src={this.props.image_src_} className="Card-image" alt="Avatar"/>
-            <div className="Card-script">
-                <h3>{this.props.username_}</h3>
+      <div className="card">
+            <img src={this.props.image_src_}  alt="Avatar"/>
+            <div >
+                <h3><a>{this.props.username_}</a>></h3>
                 <h4>{this.props.role_}</h4>
             </div>
       </div>
@@ -29,40 +33,49 @@ class Front_card extends React.Component<Front_cardProps, {}> {
 }
 
 
-export class Card extends React.Component<CardProps, {}> {
+class Back_card extends React.Component<Back_cardProps, {}> {
+  render() {
+    return (
+      <div className="">
+            <div ><a >{this.props.command_}</a></div>
+            <div ><p >{this.props.position_}</p></div>
+            <div ><p >{this.props.place_}</p></div>
+            <div ><p >{this.props.script_}</p></div>
+      </div>
+    );
+  }
+}
 
+
+export class Card extends React.Component<CardProps, IState_card> {
   constructor(props:CardProps) {
       super(props);
-      this.state = {
-      side_card : <Front_card username_={this.props.username_} role_={this.props.role_} image_src_={this.props.image_src_} />,
-      count: 0
-    //  side_card : "front_card"
-    };
+      this.state = {side_card_flag: this.props.side_};
   }
-  swap() {
-    if ({this.state.side_card} == "front_card") {
-      setState({side_card: "back_card"});
+  onSwap() :void {
+    var st:string = this.state.side_card_flag;
+    if (st == FRONT_SIDE) {
+      this.setState({side_card_flag: BACK_SIDE});
     }
-    else if ({this.state.side_card} == "back_card") {
-      setState({side_card: "front_card"});
+    else if (st == BACK_SIDE) {
+      this.setState({side_card_flag: FRONT_SIDE});
     }
   }
   render() {
-    if (side_card == "front_card") {
-      return(
-        <div className="Card-main" onClick={this.swap.bind(this)}>
-          <Front_card username_={this.props.username_} role_={this.props.role_} image_src_={this.props.image_src_} />
-        </div>
-      );
+    var st:string = this.state.side_card_flag;
+    if (st == FRONT_SIDE) {
+        return (
+          <div onClick={this.onSwap.bind(this)}>
+             <Front_card username_={this.props.username_} role_={this.props.role_} image_src_={this.props.image_src_} />,
+          </div>
+        );
     }
-    else if (side_card == "back_card") {
-      return(
-        <div className="Card-main" onClick={this.swap.bind(this)}>
+    else if (st == BACK_SIDE) {
+      return (
+        <div onClick={this.onSwap.bind(this)}>
           <Back_card command_={this.props.command_} position_={this.props.position_} place_={this.props.place_} script_={this.props.script_} />
         </div>
       );
     }
   }
-
-
 }
