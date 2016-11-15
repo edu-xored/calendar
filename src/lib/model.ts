98ac3745db736afd25a411f7ace0c7bf7e74c313
sergeyt <stodyshev@gmail.com>
2016-11-15 23:50:34 +0700

    removed organizationId from calendar entity

--- src/lib/model.ts
+++ src/lib/model.ts
@@ -42,7 +42,6 @@ export interface Calendar extends Entity {
   name: string;
   type: string;
   description: string;
-  organizationId: string;
   teamId: string;
   events?: Event[]; // only as part of API payload, actually stored in separate association table CalendarEvents
 }
