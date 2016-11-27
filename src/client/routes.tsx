61d328f65f26c0ee7bc5719db14c923429db12ca
dianavasilyeva <vasdianavas@gmail.com>
2016-11-27 13:07:12 +0600

    fixedd page header for login page (issue #11)

--- src/client/routes.tsx
+++ src/client/routes.tsx
@@ -6,13 +6,31 @@ import Blank from './pages/blank';
 import CalendarList from './pages/calendar_list';
 import Calendar from './pages/calendar';
 import Login from './pages/login';
+import PageHeader from './pageheader';
+
+class BasicLayout extends React.Component<any, {}> {
+  render() {
+    return (
+      <div>
+        <div id='header'>
+          <PageHeader/>
+        </div>
+        <div>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
 
 const Routes = (
   <Router history={history}>
-    <Route path="/" component={Home}/>
-    <Route path="/admin/calendars" component={CalendarList}/>
+    <Route path="/" component={BasicLayout}>
+      <IndexRoute component={Home}/>
+      <Route path="admin/calendars" component={CalendarList}/>
+      <Route path="calendar/*" component={Calendar}/>
+    </Route>
     <Route path="/login" component={Login}/>
-    <Route path="/calendar/*" component={Calendar}/>
     <Route path="*" component={Blank}/>
   </Router>
 );
