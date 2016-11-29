cc1b78ef875730cd76e52b16be3dae6d86c62feb
Sergey Todyshev <sergey.todyshev@xored.com>
2016-11-29 12:59:39 +0700

    removed foreign key fields

--- src/server/database/eventModel.ts
+++ src/server/database/eventModel.ts
@@ -5,9 +5,7 @@ import {Event} from "../../lib/model";
 export default makeEntityFn<Event>('event', {
   type: ORM.STRING, // TODO enum type
   comment: ORM.STRING,
-  calendarId: ORM.STRING,
   start: ORM.TIME,
   end: ORM.TIME,
-  userId: ORM.STRING,
   duration: ORM.STRING,
 });
